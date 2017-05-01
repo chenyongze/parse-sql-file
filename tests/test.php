f48a42eaed80b1040c796fd65114406a9a01933f
yongze <sapphire.php@gmail.com>
2017-05-01 08:06:36 +0800

    update test

--- tests/test.php
+++ tests/test.php
@@ -6,22 +6,22 @@ use Yongze\ParseSql\Sql;
 
 
 // 默认读取的sql语句是数组形式
-$filePath = __DIR__;
-$sql = Sql::getSqlFromFile($filePath.'/test.sql');
+$filePath = __DIR__.'/test.sql';
+$sql = Sql::getSqlFromFile($filePath);
 echo "【已数组形式返回sql语句】";
 echo "<pre>";
 var_dump($sql);
 echo "</pre>";
 
 // 读取的sql语句为一条语句
-$sql = Sql::getSqlFromFile($filePath.'/test.sql', true);
+$sql = Sql::getSqlFromFile($filePath, true);
 echo "【已单条语句形式返回sql语句】";
 echo "<pre>";
 var_dump($sql);
 echo "</pre>";
 
 // 替换表前缀
-$sql = Sql::getSqlFromFile($filePath.'/test.sql', false, array('my_' => 'me_'));
+$sql = Sql::getSqlFromFile($filePath, false, array('my_' => 'me_'));
 echo "【替换表前缀】";
 echo "<pre>";
 var_dump($sql);
